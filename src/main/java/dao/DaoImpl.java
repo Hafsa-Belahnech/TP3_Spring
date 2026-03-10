bd4be12a66dacf17c34e705b5c360abd4e56dcf8
Hafsa-Belahnech <250507787+Hafsa-Belahnech@users.noreply.github.com>
2026-03-10 14:15:37 +0000

    fix

--- src/main/java/dao/DaoImpl.java
+++ src/main/java/dao/DaoImpl.java
@@ -3,7 +3,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Profile("prod")
+@Profile("prod") //commenté si n'est pas utilisé
 public class DaoImpl implements IDao {
-    @Override public double getValue(){ return 200.0; }
+    @Override public double getValue(){ return 300.0; }
 }
