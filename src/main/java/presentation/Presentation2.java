1f2c17c79d25dd18c64cb3e6cffeb665c53fe900
Hafsa-Belahnech <250507787+Hafsa-Belahnech@users.noreply.github.com>
2026-03-10 14:12:26 +0000

    Update Presentation2.java

--- src/main/java/presentation/Presentation2.java
+++ src/main/java/presentation/Presentation2.java
@@ -9,14 +9,14 @@ public class Presentation2 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 
-        // Choix 1 (profils) : décommenter un profil pour tester
-        //ctx.getEnvironment().setActiveProfiles("dev");  //600
-        ctx.getEnvironment().setActiveProfiles("prod"); //dao = 300
+        // Choix 1 pour les profils : on décommente un profil pour le tester
+        //ctx.getEnvironment().setActiveProfiles("dev");  //400
+        //ctx.getEnvironment().setActiveProfiles("prod"); //560
         //ctx.getEnvironment().setActiveProfiles("file");  //600
-        // ctx.getEnvironment().setActiveProfiles("api");
+        // ctx.getEnvironment().setActiveProfiles("api"); //444
 
-        // Choix 2 (propriété externe) : laisser les profils vides,
-        // PropertyDrivenConfig créera un bean "dao" selon app.properties
+        // Choix 2 pour propriété externe: on laisse les profils vides
+        // PropertyDrivenConfig créera ainsi un bean "dao" selon app.properties
 
         ctx.register(Presentation2.class);
         ctx.refresh();
