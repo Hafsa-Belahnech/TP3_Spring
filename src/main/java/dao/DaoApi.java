177d9f719a9d4fe15cd47386912a798c911105c8
Hafsa-Belahnech <250507787+Hafsa-Belahnech@users.noreply.github.com>
2026-03-10 14:13:47 +0000

    Update DaoApi.java

--- src/main/java/dao/DaoApi.java
+++ src/main/java/dao/DaoApi.java
@@ -3,7 +3,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component("daoApi")
-//@Profile("api")
+@Profile("api") //on le commente si on n'utilise pas cette classe
 public class DaoApi implements IDao {
     @Override public double getValue(){ return 222.0; }
 }
